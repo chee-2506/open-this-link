5116173ad0eb15bf2c41077d6ee7ec2163c78eaa
chee-2506 <86784520+chee-2506@users.noreply.github.com>
2021-07-13 23:10:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'xin chào người yêu của em !',
+    introDesc: 'i'm a bee, do you want to be my honey ?'
+    btnIntro: 'có hoặc kó',
+    title: 'khi nhìn vào mắt anh, em nhận ra sao trên trời đều là giả =))',
+    desc: 'anh sẽ là "ngôi sao" sáng của riêng em chứ ?',
+    btnYes: 'ukiiii babiii <33',
+    btnNo: 'à ùm...nâu nâu nâu :<<',
+    question:'Tuyết Nhi,em chỉ muốn nói là em yêu anh, yêu anh rất nhiều. và em mong ước rằng hai mình sẽ bên nhau mãi mãi luôn nha anh <3',
+    btnReply: 'đồng ý đi cho bóp dzú. nói thiệt hong đùa đâu :>>>',
+    reply: 'cóaaaa <33333333',
+    mess: 'hỡi người yêu cuer em ~~ ',
+    messDesc: 'gặp nhau cho em hun má nha...hun cả môi nữa :>>>> .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
